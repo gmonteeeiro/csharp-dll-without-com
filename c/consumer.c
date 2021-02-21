bacd04226feae018ec8f8bb1776e76280c7d12ea
Gabriel Monteiro <gmonteeeiro@gmail.com>
2021-02-21 18:55:55 -0300

    Cria métodos para consumir DLL

--- c/consumer.c
+++ c/consumer.c
@@ -2,25 +2,55 @@
 #include <stdlib.h>
 #include <windows.h>
 
+void SumTwoNumbers(HINSTANCE hDLL){
+    typedef void (*AllocTwoNumbers)(int number1, int number2);
+    typedef void (*GetSum)(int *result);
+
+    ((AllocTwoNumbers)GetProcAddress(hDLL, "AllocTwoNumbers"))(14, 7);
+
+    int result;
+    ((GetSum)GetProcAddress(hDLL, "GetSum"))(&result);
+
+    printf("%d", result);
+}
+
+void SendAndReturnString(HINSTANCE hDLL, char* string){
+    typedef void (*AllocText)(char *string);
+    typedef void (*GetText)(char **string);
+
+    ((AllocText)GetProcAddress(hDLL, "AllocText"))(string);
+
+    char* stringReturn;
+    ((GetText)GetProcAddress(hDLL, "GetText"))(&stringReturn);
+
+    printf("%s", stringReturn);
+}
+
+void GetCSharpString(HINSTANCE hDLL){
+    typedef void (*GetString)(char **string);
+
+    char* stringReturn;
+    ((GetString)GetProcAddress(hDLL, "GetString"))(&stringReturn);
+
+    printf("%s", stringReturn);
+}
+
+void ShowError(HINSTANCE hDLL, char* error){
+    typedef void (*ShowError)(char *string);
+    ((ShowError)GetProcAddress(hDLL, "ShowError"))(error);
+}
+
 int main(){
-
-    typedef void (*alloc)();
-    typedef void (*free)();
-    typedef TCHAR* (*getMsg)();
-
     HINSTANCE hDLL = LoadLibrary("csharp-dll-without-com.dll");
+
     if (hDLL != NULL)
     {
-        ((alloc)GetProcAddress(hDLL, "alloc"))();
+        // SumTwoNumbers(hDLL);
+        // SendAndReturnString(hDLL, "Texto enviado pelo C");
+        GetCSharpString(hDLL);
+        // ShowError(hDLL, "Teste de erro");
 
-        auto obj = (getMsg) GetProcAddress(hDLL, "getMsg");
-        if(obj){
-            TCHAR* teste = obj();
-        }
-
-        ((free)GetProcAddress(hDLL, "free"))();
         FreeLibrary(hDLL);
-
     }   
 
     return 0;
